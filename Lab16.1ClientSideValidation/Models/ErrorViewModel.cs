77c05b45137acbba6229329803791f2e0d8de6a9
Phillips <verkeiac@yahoo.com>
2021-06-15 13:25:01 -0400

    first commit to lab 16.1

---
+++ Lab16.1ClientSideValidation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab16._1ClientSideValidation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
